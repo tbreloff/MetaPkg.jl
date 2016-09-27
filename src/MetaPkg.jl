c3d57c1dec77d65ed11ff870330dec6b678b8d1d
Thomas Breloff <tom@breloff.com>
2016-09-27 12:06:53 -0400

    meta

--- src/MetaPkg.jl
+++ src/MetaPkg.jl
@@ -13,9 +13,29 @@ type MetaSpec
     branch::Vector{Package}
 end
 
+# maps spec_name --> MetaSpec
 const _specs = Dict{String, MetaSpec}
 
 function load_meta(metaname::String, filename::String)
+    f = open(filename)
+    spec = nothing
+    for l in eachline(f)
+        tokens = split(strip(l))
+        isempty(tokens) && continue
+
+        # process the "julia 0.5-" line... setting the spec
+        if first(tokens) == "julia"
+            version = VersionNumber(tokens[2])
+            spec = if VERSION >= version
+                get!(_specs, metaname, MetaSpec(metaname))
+            else
+                nothing
+            end
+
+        # for other lines, we only care to process if this version applies
+        elseif spec != nothing
+        end
+    end
 end
 
 end # module
